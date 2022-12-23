6e58662bb8ccfbe9f36e3935d568836c1e76fb7c
zopalmer14 <zopalmer14@gmail.com>
2022-12-22 23:27:49 -0500

    fixed navbar trigger; still need to add on hover styling changes

--- dynamic.js
+++ dynamic.js
@@ -6,7 +6,9 @@ window.onscroll = function() {
 }
 
 function scrollFunction() {
-  if (document.body.scrollTop > 600 || document.documentElement.scrollTop > 600) {
+  // check the coordinates of the background to see if it has exited the viewport
+  var background_pos = document.getElementById("hoh-background").getBoundingClientRect();
+  if (background_pos.bottom <= 0) {
     document.getElementById("magic-navbar").style.top = "0";
   } else {
     document.getElementById("magic-navbar").style.top = "-200px";
