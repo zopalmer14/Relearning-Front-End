915f0e42206503217763021166aeccf1f551d2c2
zopalmer14 <zopalmer14@gmail.com>
2022-12-23 21:06:44 -0500

    feat: added JS handling of mouseover / mouseexit of navbar

--- dynamic.js
+++ dynamic.js
@@ -1,8 +1,25 @@
 
 // JS file to allow dynamic animations / processes
 
-window.onscroll = function() {
+function setup() {
+  /* check for the window position relative to the header img whenever the user scrolls */  
+  window.onscroll = function() {
     scrollFunction();
+  }
+
+  /* when the user mouses over the navbar, we need to change the styling */
+  var navbars = document.getElementsByClassName("navbar");
+  for (var i = 0; i < navbars.length; i++) {
+    /* mouseover function */
+    navbars[i].onmouseover = function() {
+      navbarEnter();
+    }
+
+    /* mouseleave function */
+    navbars[i].onmouseleave = function() {
+      navbarExit();
+    }
+  }
 }
 
 function scrollFunction() {
@@ -12,5 +29,53 @@ function scrollFunction() {
     document.getElementById("magic-navbar").style.top = "0";
   } else {
     document.getElementById("magic-navbar").style.top = "-200px";
+  }
+}
+
+function navbarEnter() {
+  /* change the background color of the banner and the navbar itself */
+  var banner = document.getElementById("banner");
+  banner.style.background = '#f2f2f2';
+
+  var navbars = document.getElementsByClassName("navbar");
+  for (var i = 0; i < navbars.length; i++) {
+    navbars[i].style.background = '#024F0B';
+  }
+
+  /* change the color of the text within the navbar */
+  var name = document.getElementById("name");
+  name.style.color = '#f2f2f2';
+
+  var nav_links = document.getElementsByClassName("nav-link");
+  for (var i = 0; i < nav_links.length; i++) {
+    nav_links[i].style.color = '#f2f2f2';
+    console.log(nav_links[i]);
+    if (nav_links[i].firstElementChild != null) {
+      console.log(nav_links[i].firstElementChild);
+      nav_links[i].firstElementChild.style.color = '#f2f2f2';
+    }
+  }
+}
+
+function navbarExit() {
+  /* change the background color of the banner and the navbar itself */
+  var banner = document.getElementById("banner");
+  banner.style.background = '#024F0B';
+
+  var navbars = document.getElementsByClassName("navbar");
+  for (var i = 0; i < navbars.length; i++) {
+    navbars[i].style.background = '#f2f2f2';
+  }
+
+  /* change the color of the text within the navbar */
+  var name = document.getElementById("name");
+  name.style.color = '#333333';
+
+  var nav_links = document.getElementsByClassName("nav-link");
+  for (var i = 0; i < nav_links.length; i++) {
+    nav_links[i].style.color = '#333333';
+    if (nav_links[i].firstElementChild != null) {
+      nav_links[i].firstElementChild.style.color = '#333333';
+    }
   }
 }
