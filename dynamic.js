848cc0930b39daec0ffe9f30eecd1734ce775852
zopalmer14 <zopalmer14@gmail.com>
2022-12-25 00:00:21 -0500

    fixed redirect link styling and removed unnecessary JS

--- dynamic.js
+++ dynamic.js
@@ -6,20 +6,6 @@ function setup() {
   window.onscroll = function() {
     scrollFunction();
   }
-
-  /* when the user mouses over the navbar, we need to change the styling */
-  var navbars = document.getElementsByClassName("navbar");
-  for (var i = 0; i < navbars.length; i++) {
-    /* mouseover function */
-    navbars[i].onmouseover = function() {
-      navbarEnter();
-    }
-
-    /* mouseleave function */
-    navbars[i].onmouseleave = function() {
-      navbarExit();
-    }
-  }
 }
 
 function scrollFunction() {
@@ -31,52 +17,6 @@ function scrollFunction() {
       document.getElementById("magic-navbar").style.top = "0";
     } else {
       document.getElementById("magic-navbar").style.top = "-200px";
-    }
-  }
-}
-
-function navbarEnter() {
-  /* change the background color of the banner and the navbar itself */
-  var banner = document.getElementById("banner");
-  banner.style.background = '#8a8a8a';
-
-  var navbars = document.getElementsByClassName("navbar");
-  for (var i = 0; i < navbars.length; i++) {
-    navbars[i].style.background = '#024F0B';
-  }
-
-  /* change the color of the text within the navbar */
-  var name = document.getElementById("name");
-  name.style.color = '#f2f2f2';
-
-  var nav_links = document.getElementsByClassName("nav-link");
-  for (var i = 0; i < nav_links.length; i++) {
-    nav_links[i].style.color = '#f2f2f2';
-    if (nav_links[i].firstElementChild != null) {
-      nav_links[i].firstElementChild.style.color = '#f2f2f2';
-    }
-  }
-}
-
-function navbarExit() {
-  /* change the background color of the banner and the navbar itself */
-  var banner = document.getElementById("banner");
-  banner.style.background = '#024F0B';
-
-  var navbars = document.getElementsByClassName("navbar");
-  for (var i = 0; i < navbars.length; i++) {
-    navbars[i].style.background = '#f2f2f2';
-  }
-
-  /* change the color of the text within the navbar */
-  var name = document.getElementById("name");
-  name.style.color = '#333333';
-
-  var nav_links = document.getElementsByClassName("nav-link");
-  for (var i = 0; i < nav_links.length; i++) {
-    nav_links[i].style.color = '#333333';
-    if (nav_links[i].firstElementChild != null) {
-      nav_links[i].firstElementChild.style.color = '#333333';
     }
   }
 }
